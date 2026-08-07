03c36c0505a2915e7302a50b8a286b680cabbe86
karan <karanscy@gmail.com>
2026-08-07 00:53:49 -0400

    added jwt files, a new file auth.py is added

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.models.users import Loginschema, SignupSchema, User
 from app.database import get_db
+from app.hashing import verify_password, hash_password
+from app.auth import create_access_token
+
+
+
+
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Wlecome to the App"}
 
 @app.post("/login")
 async def userlogin(user: Loginschema, db: Session = Depends(get_db)):
@@ -15,10 +18,12 @@ async def userlogin(user: Loginschema, db: Session = Depends(get_db)):
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if db_user.pwd != user.pwd:
+    if not verify_password(user.pwd, db_user.pwd):
         raise HTTPException(status_code=401, detail="Incorrect password")
 
-    return {"message": "login successful", "user_id": db_user.id, "email": db_user.email}
+    access_token = create_access_token(data = {"user_id": db_user.id, "email": db_user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/admin/users")
 async def get_all_users(db: Session = Depends(get_db)):
@@ -28,7 +33,7 @@ async def get_all_users(db: Session = Depends(get_db)):
 
 @app.post("/signup")
 async def signupuser(newuser: SignupSchema, db: Session = Depends(get_db)):
-    new_user = User(email=newuser.email, pwd= newuser.password, name = newuser.name)
+    new_user = User(email=newuser.email, pwd= hash_password(newuser.password), name = newuser.name)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
